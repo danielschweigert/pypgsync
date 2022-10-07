69b272e534d9bb32bd92ef4f46cf6f1b2f3713c8
Daniel Schweigert <dan.schweigert@gmail.com>
2022-10-07 14:17:30 -0700

    feat: add function to delete records by primary key

---
+++ tests/util/test_delete.py
@@ -0,0 +1,50 @@
+"""
+Unit test for delete functions
+"""
+from typing import List
+import pytest
+from pypgsync.util.query import get_column_values
+from pypgsync.util.create import insert_records
+from pypgsync.util.delete import delete_record_by_primary_key_value
+
+
+@pytest.mark.usefixtures("con_source")
+def test_delete_record_by_primary_key_value(con_source):
+
+    table_name = "table_a"
+    primary_key = "id"
+
+    cur_source = con_source.cursor()
+    remaining_pks = get_column_values(cur=cur_source, table_name=table_name,
+                                      column_name=primary_key)
+    assert remaining_pks == []
+
+    records = [
+        {"id": 1, "my_varchar": "a", "my_int": 10},
+        {"id": 2, "my_varchar": "b", "my_int": 11},
+        {"id": 3, "my_varchar": "c", "my_int": 12},
+        {"id": 4, "my_varchar": "d", "my_int": 13},
+        {"id": 5, "my_varchar": "e", "my_int": 14},
+    ]
+    insert_records(con=con_source, table_name=table_name, records=records)
+    remaining_pks = get_column_values(cur=cur_source, table_name=table_name,
+                                      column_name=primary_key)
+    assert remaining_pks == [1, 2, 3, 4, 5]
+
+    delete_record_by_primary_key_value(con=con_source, table_name=table_name,
+                                       primary_key=primary_key, primary_key_value=3)
+    remaining_pks = get_column_values(cur=cur_source, table_name=table_name,
+                                      column_name=primary_key)
+    assert remaining_pks == [1, 2, 4, 5]
+
+    delete_record_by_primary_key_value(con=con_source, table_name=table_name,
+                                       primary_key=primary_key, primary_key_value=3)
+    remaining_pks = get_column_values(cur=cur_source, table_name=table_name,
+                                      column_name=primary_key)
+    assert remaining_pks == [1, 2, 4, 5]
+
+    delete_record_by_primary_key_value(con=con_source, table_name=table_name,
+                                       primary_key=primary_key, primary_key_value=4)
+    remaining_pks = get_column_values(cur=cur_source, table_name=table_name,
+                                      column_name=primary_key)
+    assert remaining_pks == [1, 2, 5]
